6ef97a0a474721dd2b925fa4f7aac4bd0e7897d8
Wassim <wassim.b@turing.com>
2022-02-12 23:09:08 +0100

    Update
    Signed-off-by: Wassim <wassim.b@turing.com>

--- back-end/midalwares/jwt_validate.js
+++ back-end/midalwares/jwt_validate.js
@@ -1,19 +1,45 @@
 const jwt = require('jsonwebtoken');
 const TOKEN_SECRET = 'sqldklfqlqs545';
 
-module.exports = function (req,res,next){
-  console.log(req);
+function userMidalware(req, res, next) {
+    console.log(req);
     const authHeader = req.headers['authorization'];
-    if(!authHeader) return res.status(401).send('Access Denied');
+    if (!authHeader) return res.status(401).send('Access Denied');
     try {
-    jwt.verify(authHeader, TOKEN_SECRET,(err,user)=>{
-if(err) return res.sendStatus(403)
-req.user = user;
+      jwt.verify(authHeader, TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
         next();
-        });
-        
+      });
+  
     } catch (error) {
       console.log(error);
-      return res.status(401).send('Invalid Token') ;
+      return res.status(401).send('Invalid Token');
     }
+}
+
+function adminMidalware(req, res, next) {
+  console.log(req);
+  const authHeader = req.headers['authorization'];
+  if (!authHeader) return res.status(401).send('Access Denied');
+  try {
+    jwt.verify(authHeader, TOKEN_SECRET, (err, user) => {
+      if (err) return res.sendStatus(403)
+      if (user.type === "admin") {
+        req.user = user;
+        next();
+      } else {
+        res.status(401).send('Not Admin: Access Denied');
+      }
+    });
+
+  } catch (error) {
+    console.log(error);
+    return res.status(401).send('Invalid Token');
+  }
+}
+
+module.exports = {
+  userMidalware,
+  adminMidalware
 }
--- front end/src/app/service/participant.service.ts
+++ front end/src/app/service/participant.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import {Participant} from "../../participant"
-import {HttpClient,HttpHeaders} from '@angular/common/http';
+import { Participant } from "../../participant"
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type':  'application/json',
-    
+    'Content-Type': 'application/json',
+
   })
 };
 
@@ -16,11 +16,11 @@ const httpOptions = {
 })
 export class ParticipantService {
 
- 
+
   constructor(private http: HttpClient) { }
 
-   getParticipants():Observable<Participant[]> {
-     const apiUrl = "http://localhost:3000/user/afficher_participant";
+  getParticipants (): Observable<Participant[]> {
+    const apiUrl = "http://localhost:3000/user/afficher_participant";
     console.log(this.http.get<Participant[]>(apiUrl))
     return this.http.get<Participant[]>(apiUrl);
   }
@@ -29,24 +29,34 @@ export class ParticipantService {
   //   return this.http.delete<Task>(url)
   // }
 
-  addParticipant(participant:Participant):Observable<Participant>{
+  addParticipant (participant: Participant): Observable<Participant> {
     const apiUrl = "http://localhost:3000/user/inscrit_participant";
     console.log(participant)
-    return this.http.post<Participant>(apiUrl,participant)
+    return this.http.post<Participant>(apiUrl, participant)
   }
- 
-   
-   
-    modifyParticipant(participant:Participant):Observable<Participant>{
-      const apiUrl = `http://localhost:3000/user/modifier_user/${participant._id}`;
-      console.log("task",participant)
-      // const body = { title: 'Angular PUT Request Example' };
-      // const url=`${apiUrl}/${participant.id}`
-      return this.http.put<Participant>(apiUrl,participant)  
+
+
+
+  modifyParticipant (participant: Participant): Observable<Participant> {
+    const apiUrl = `http://localhost:3000/user/modifier_user/${participant._id}`;
+    console.log("task", participant)
+    // const body = { title: 'Angular PUT Request Example' };
+    // const url=`${apiUrl}/${participant.id}`
+    return this.http.put<Participant>(apiUrl, participant)
   }
-  getuser(id:any){
+  getuser (id: any) {
     const apiUrl = "http://localhost:3000/user/afficher_seuluser";
     console.log(this.http.get<Participant[]>(`${apiUrl}/${id}`))
     return this.http.get<Participant[]>(`${apiUrl}/${id}`);
   }
+
+  register (type: any, cin1: any, cin2: any, titre: any) {
+    console.log('jwt', localStorage.getItem('jwt') || '');
+    const apiUrl = "http://localhost:3000/user/register";
+    return this.http.post<any>(apiUrl, { type, cin1, cin2, titre }, { headers: { authorization: localStorage.getItem('jwt') || '' } }).subscribe(data => {
+      console.log(data);
+    })
+  }
+
+
 }
